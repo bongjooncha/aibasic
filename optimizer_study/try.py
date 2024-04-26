ae094cc496d8e6d41e769207ca67076d2d7cc404
bongjooncha <cbj4133@naver.com>
2024-04-26 14:17:28 +0900

    sgd,mgd

--- optimizer_study/try.py
+++ optimizer_study/try.py
@@ -1,3 +1,8 @@
 import numpy as np
-theta_sgd = np.random.randn(2, 1)  # 무작위 초기화
-print(theta_sgd)
+
+m = 100
+X = 2 * np.random.rand(100, 1) 
+X_b = np.c_[np.ones((100, 1)), X]
+shuffled_indices = np.random.permutation(m)
+print(X_b)
+print(X_b[shuffled_indices])

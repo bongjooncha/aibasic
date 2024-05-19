1b526c2bae93d7c5f5d472010fc8e8fe1cc25033
hiddenbebb <cbj4133@naver.com>
2024-05-19 20:45:08 +0900

    mse 작성 시작

--- lossfunction_study/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # plt.plot(theta_mse_path[:i+1, 0], theta_mse_path[:i+1, 1], 'r-', label="MSE")
-    # plt.scatter(theta_mse_path[i, 0], theta_mse_path[i, 1], color='red', s=10, marker='o')
-
-    # plt.plot(theta_mae_path[:i+1, 0], theta_mae_path[:i+1, 1], 'g-', label="MAE")
-   
